7e76161d1dfd62e01160da05acdeab8aaafceaa7
Venkat <venkat@Konas-MacBook-Air.local>
2026-08-11 19:19:00 +0530

    seconfd commit

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
@@ -7,4 +7,5 @@ export default defineConfig({
   resolve: {
     dedupe: ['react', 'react-dom'],
   },
+  base: "/SaiLakshmi-portfolio "
 })
